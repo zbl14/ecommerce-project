d76173846fa70f0247d618ebee7ef5768412cc20
Zhibin Liang <ifthereisoneday@gmail.com>
2023-03-22 23:26:54 -0400

    add CrossOrigin

--- spring-boot-ecommerce/src/main/java/com/zliang14/ecommerce/dao/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/zliang14/ecommerce/dao/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.zliang14.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.zliang14.ecommerce.entity.Product;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
