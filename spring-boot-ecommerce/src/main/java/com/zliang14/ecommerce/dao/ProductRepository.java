40ea55593554715c3d956b020808e68efd0a5575
Zhibin Liang <ifthereisoneday@gmail.com>
2023-03-24 01:04:52 -0400

    search for products by keyword

--- spring-boot-ecommerce/src/main/java/com/zliang14/ecommerce/dao/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/zliang14/ecommerce/dao/ProductRepository.java
@@ -11,4 +11,6 @@ import com.zliang14.ecommerce.entity.Product;
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
   Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+
+  Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
 }
