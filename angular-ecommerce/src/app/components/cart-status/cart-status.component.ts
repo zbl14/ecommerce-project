5a532621562ac73a0e5a01dad61de8eaaff9bf53
Zhibin Liang <ifthereisoneday@gmail.com>
2023-05-01 14:52:43 -0400

    subscribe to CartService

--- angular-ecommerce/src/app/components/cart-status/cart-status.component.ts
+++ angular-ecommerce/src/app/components/cart-status/cart-status.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-status',
   templateUrl: './cart-status.component.html',
-  styleUrls: ['./cart-status.component.css']
+  styleUrls: ['./cart-status.component.css'],
 })
 export class CartStatusComponent {
+  totalPrice: number = 0.0;
+  totalQuantity: number = 0;
 
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.updateCartStatus();
+  }
+
+  updateCartStatus() {
+    // subscribe to the cart totalPrice and totalQuantity
+    this.cartService.totalPrice.subscribe((data) => (this.totalPrice = data));
+    this.cartService.totalQuantity.subscribe(
+      (data) => (this.totalQuantity = data)
+    );
+  }
 }
