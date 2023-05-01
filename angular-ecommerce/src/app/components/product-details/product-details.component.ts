e4633e8b63b969cbf308034a4485ac14cd3c4c83
Zhibin Liang <ifthereisoneday@gmail.com>
2023-05-01 15:13:45 -0400

    add event handler addToCart

--- angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -13,6 +15,7 @@ export class ProductDetailsComponent {
 
   constructor(
     private productService: ProductService,
+    private cartService: CartService,
     private route: ActivatedRoute
   ) {}
 
@@ -28,5 +31,14 @@ export class ProductDetailsComponent {
     this.productService.getProduct(theProductId).subscribe((data) => {
       this.product = data;
     });
+  }
+
+  addToCart() {
+    console.log(
+      `Adding to cart: ${this.product.name}, ${this.product.unitPrice}`
+    );
+
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
   }
 }
