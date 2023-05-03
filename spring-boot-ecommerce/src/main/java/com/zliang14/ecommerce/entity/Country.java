fbaa6b2303fb301f5e293d433e5850961addf770
Zhibin Liang <ifthereisoneday@gmail.com>
2023-05-02 22:38:21 -0400

    develop JPA Entities

---
+++ spring-boot-ecommerce/src/main/java/com/zliang14/ecommerce/entity/Country.java
@@ -0,0 +1,34 @@
+package com.zliang14.ecommerce.entity;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "country")
+@Getter
+@Setter
+public class Country {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private int id;
+
+  @Column(name = "code")
+  private String code;
+
+  @Column(name = "name")
+  private String name;
+
+  @OneToMany(mappedBy = "country")
+  private List<State> state;
+}
