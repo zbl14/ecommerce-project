b9de5061057be0e8e7504d780460b2b1e4d4ff05
Zhibin Liang <ifthereisoneday@gmail.com>
2023-05-05 15:18:38 -0400

    init login

--- angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { CartStatusComponent } from './components/cart-status/cart-status.compon
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: 'checkout', component: CheckoutComponent },
@@ -38,6 +39,7 @@ const routes: Routes = [
     CartStatusComponent,
     CartDetailsComponent,
     CheckoutComponent,
+    LoginComponent,
   ],
   imports: [
     RouterModule.forRoot(routes),
