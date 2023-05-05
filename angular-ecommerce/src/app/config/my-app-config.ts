70e0374253c521af49134ff1f1a73c0fc0289ecb
Zhibin Liang <ifthereisoneday@gmail.com>
2023-05-05 15:07:36 -0400

    set up app config for OpenID connect

---
+++ angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  odic: {
+    clientId: '0oa9exnqmfYpR9bL75d7',
+    issuer: 'https://dev-29548176.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
