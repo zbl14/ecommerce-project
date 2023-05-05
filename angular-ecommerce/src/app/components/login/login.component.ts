3fbd9d3a202d19ecafbef106b7eae8da57dd047f
Zhibin Liang <ifthereisoneday@gmail.com>
2023-05-05 15:35:52 -0400

    integrate Okta Sign-In Widget

--- angular-ecommerce/src/app/components/login/login.component.ts
+++ angular-ecommerce/src/app/components/login/login.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+import OktaSigIn from '@okta/okta-signin-widget';
+
+import myAppConfig from 'src/app/config/my-app-config';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  oktaSignin: any;
 
+  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) {
+    this.oktaSignin = new OktaSigIn({
+      logo: 'assets/images/logo.jpg',
+      baseUrl: myAppConfig.odic.issuer.split('/oauth2')[0],
+      clientId: myAppConfig.odic.clientId,
+      redirectUri: myAppConfig.odic.redirectUri,
+      authParams: {
+        pkce: true,
+        issuer: myAppConfig.odic.issuer,
+        scopes: myAppConfig.odic.scopes,
+      },
+    });
+  }
+
+  ngOnInit(): void {
+    this.oktaSignin.remove();
+
+    this.oktaSignin.renderEl(
+      {
+        el: '#okta-sign-in-widget',
+      },
+      (response: any) => {
+        if (response.status === 'SUCCESS') {
+          this.oktaAuth.signInWithRedirect();
+        }
+      },
+      (error: any) => {
+        throw error;
+      }
+    );
+  }
 }
