355a6e3b7e54a62b909e9b4b1d8f579f6116bb3c
Zhibin Liang <ifthereisoneday@gmail.com>
2023-05-05 15:59:55 -0400

    login/logout buttons

--- angular-ecommerce/src/app/components/login-status/login-status.component.ts
+++ angular-ecommerce/src/app/components/login-status/login-status.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { OKTA_AUTH, OktaAuthStateService } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
 
 @Component({
   selector: 'app-login-status',
   templateUrl: './login-status.component.html',
-  styleUrls: ['./login-status.component.css']
+  styleUrls: ['./login-status.component.css'],
 })
-export class LoginStatusComponent {
+export class LoginStatusComponent implements OnInit {
+  isAuthenticated: boolean = false;
+  userFullName: String = '';
 
+  constructor(
+    private oktaAuthService: OktaAuthStateService,
+    @Inject(OKTA_AUTH) private oktaAuth: OktaAuth
+  ) {}
+
+  ngOnInit(): void {
+    // subscribe to authentication state changes
+    this.oktaAuthService.authState$.subscribe((result) => {
+      this.isAuthenticated = result.isAuthenticated!;
+      this.getUserDetails();
+    });
+  }
+
+  getUserDetails() {
+    if (this.isAuthenticated) {
+      // fetch the logged in user details
+
+      // user full name is exposed as a property name
+      this.oktaAuth.getUser().then((res) => {
+        this.userFullName = res.name as string;
+      });
+    }
+  }
+
+  logout() {
+    // termiantes the session with Okta and remove current tokens
+    this.oktaAuth.signOut();
+  }
 }
