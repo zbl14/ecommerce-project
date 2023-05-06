bf059ee0c5b3455b0cce5a18ca9a56c25b0c77e0
Zhibin Liang <ifthereisoneday@gmail.com>
2023-05-06 16:28:03 -0400

    Modify spring data REST Detection Strategy, customer can only be interal use

--- spring-boot-ecommerce/src/main/java/com/zliang14/ecommerce/dao/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/zliang14/ecommerce/dao/ProductRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.zliang14.ecommerce.entity.Product;
 
+@RepositoryRestResource
 public interface ProductRepository extends JpaRepository<Product, Long> {
   Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 
