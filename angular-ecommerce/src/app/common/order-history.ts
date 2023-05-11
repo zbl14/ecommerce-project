0d3f913a658d743a820477b7b276bb62203100c3
Zhibin Liang <ifthereisoneday@gmail.com>
2023-05-11 11:39:28 -0400

    create orderhistory class

--- angular-ecommerce/src/app/common/order-history.ts
+++ angular-ecommerce/src/app/common/order-history.ts
@@ -1,2 +1,9 @@
 export class OrderHistory {
+  constructor(
+    public id: string,
+    public orderTrackingNumber: string,
+    public totalPrice: number,
+    public totalQuantity: number,
+    public dateCreated: Date
+  ) {}
 }
