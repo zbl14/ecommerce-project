f6a8f8aee1a96aec0c940b3fc24f7e440f434f68
Zhibin Liang <ifthereisoneday@gmail.com>
2023-05-11 11:58:45 -0400

    display order history

--- angular-ecommerce/src/app/components/order-history/order-history.component.ts
+++ angular-ecommerce/src/app/components/order-history/order-history.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrderHistory } from 'src/app/common/order-history';
+import { OrderHistoryService } from 'src/app/services/order-history.service';
 
 @Component({
   selector: 'app-order-history',
   templateUrl: './order-history.component.html',
-  styleUrls: ['./order-history.component.css']
+  styleUrls: ['./order-history.component.css'],
 })
-export class OrderHistoryComponent {
+export class OrderHistoryComponent implements OnInit {
+  orderHistoryList: OrderHistory[] = [];
+  storage: Storage = sessionStorage;
 
+  constructor(private orderHistoryService: OrderHistoryService) {}
+
+  ngOnInit(): void {
+    this.handleOrderHistory();
+  }
+
+  handleOrderHistory() {
+    // read the email from browser storage
+    const theEmail = JSON.parse(this.storage.getItem('userEmail')!);
+
+    // retrieve data from the service
+    this.orderHistoryService.getOrderHistory(theEmail).subscribe((data) => {
+      this.orderHistoryList = data._embedded.orders;
+    });
+  }
 }
