d4b0b116a903214fc699044fd0ec986c58370e43
Zhibin Liang <ifthereisoneday@gmail.com>
2023-05-12 14:33:16 -0400

    declaration file for stripe

---
+++ angular-ecommerce/src/typings.d.ts
@@ -0,0 +1,5 @@
+// TypeScript declaration file provides type info for JavaScript libraries
+// Helps IDEs and compilers to leverage type info of JavaScript libraries
+
+declare var Stripe: any;
+declare var elements: any;
