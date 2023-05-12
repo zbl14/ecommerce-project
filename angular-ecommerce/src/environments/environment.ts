1f2030430b1e7a520e55b0479074823a3183628e
Zhibin Liang <ifthereisoneday@gmail.com>
2023-05-12 14:33:26 -0400

    add publishable key

--- angular-ecommerce/src/environments/environment.ts
+++ angular-ecommerce/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
   happyShopApiUrl: 'https://localhost:8443/api',
+  stripePublishableKey:
+    'pk_test_51N6zEPDu5I6FlKBfH2FLAPRlEHJDKn5VINna6uoLmuleRhVTK8tNtGxvtQ0j4WezokZ5tcNPJB1nCPQtUPlYMjvf00JMmek0PN',
 };
