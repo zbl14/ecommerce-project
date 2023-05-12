5a114722e2807339c817547d151043bd1893871e
Zhibin Liang <ifthereisoneday@gmail.com>
2023-05-11 22:49:19 -0400

    update redirectUri with https

--- angular-ecommerce/src/app/config/my-app-config.ts
+++ angular-ecommerce/src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oa9exnqmfYpR9bL75d7',
     issuer: 'https://dev-29548176.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
 };
