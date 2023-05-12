04599d02db79bdef72f2ed24a5e1fc19270168a8
Zhibin Liang <ifthereisoneday@gmail.com>
2023-05-12 16:10:38 -0400

    BUGFIX: clear the sessionStorage after checkout

--- angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -298,6 +298,7 @@ export class CheckoutComponent implements OnInit {
     // reset the form
     this.checkoutFormGroup.reset();
 
+    sessionStorage.clear();
     // navigate back to the products page
     this.router.navigateByUrl('/products');
   }
