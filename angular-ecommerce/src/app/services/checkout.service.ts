12e7242140d5c004c10eea814f9f6d43d3a122f3
Zhibin Liang <ifthereisoneday@gmail.com>
2023-05-12 14:38:24 -0400

    createPaymentIntent

--- angular-ecommerce/src/app/services/checkout.service.ts
+++ angular-ecommerce/src/app/services/checkout.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Purchase } from '../common/purchase';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { PaymentInfo } from '../common/payment-info';
 
 @Injectable({
   providedIn: 'root',
@@ -10,9 +11,16 @@ import { environment } from 'src/environments/environment';
 export class CheckoutService {
   private purchaseUrl = environment.happyShopApiUrl + '/checkout/purchase';
 
+  private paymentIntenUrl =
+    environment.happyShopApiUrl + '/checkout/payment-intent';
+
   constructor(private httpClient: HttpClient) {}
 
   placeOrder(purchase: Purchase): Observable<any> {
     return this.httpClient.post<Purchase>(this.purchaseUrl, purchase);
+  }
+
+  createPaymentIntent(paymentInfo: PaymentInfo): Observable<any> {
+    return this.httpClient.post<PaymentInfo>(this.paymentIntenUrl, paymentInfo);
   }
 }
